1a1c7c7d7d860396f2a1929a3eeec99e0e82680a
dawittamene <dawittamene559@gmail.com>
2023-08-29 20:01:12 +0300

    navbar

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views 
 
 urlpatterns = [
-    path('', views.store, name="home"),
+    path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     
