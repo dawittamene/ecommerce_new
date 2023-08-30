416807e9bf10e1e21729de96f6c7ca86ef600eeb
dawittamene <dawittamene559@gmail.com>
2023-08-30 23:22:34 +0300

    commit

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    #climage = models.ImageField(null=True, blank=True)
+    #mage = models.ImageField(null=True, blank=True)
     def __str__(self):
         return self.name
     
