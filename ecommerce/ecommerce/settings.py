3f7eda71bfcd6a31eb2b186bee3307f6543e4465
dawittamene <dawittamene559@gmail.com>
2023-08-30 22:11:39 +0300

    models create

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -121,7 +121,7 @@ import os
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =[
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .models import *
 
 
 # Create your views here.
 def store(request):
-    context = {}
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
     context = {}
