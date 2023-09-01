c7fcadd899333e75115ffafad595f9226dba4b66
dawittamene <dawittamene559@gmail.com>
2023-09-01 23:09:18 +0300

    add js files

--- ecommerce/store/Static/js/cart.js
+++ ecommerce/store/Static/js/cart.js
@@ -1,1 +1,19 @@
-console.log('hello, world')
+var updatBtns = document.getElementsByClassName('update-cart')
+
+for(i=0; i < updatBtns.length; i++){
+    updatBtns[i].addEventListener('click', function(){
+        var productID = this.dataset.product
+        var action = this.dataset.action
+        console.log('productID:',productID, 'action:',action)
+
+        console.log('USER:', user)
+
+        if(user === 'AnonymousUser'){
+            console.log('user is not login')
+
+        }else{
+            console.log('user is authenticated, sending data....')
+        }
+    })
+
+}
