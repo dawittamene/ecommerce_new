a1d6c841b60ed5e13f968f3a4daa96ec9bf77a05
dawittamene <dawittamene559@gmail.com>
2023-09-04 23:59:43 +0300

    commit

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -8,8 +8,23 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def store(request):
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,create = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+        
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+        cartItems =order['get_cart_items']
+    
+    
+    
+    
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products, 'cartItems':cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -17,12 +32,15 @@ def cart(request):
         customer = request.user.customer
         order,create = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+        
         
     else:
         items = []
         order = {'get_cart_items': 0, 'get_cart_total': 0}
+        cartItems =order['get_cart_items']
         
-    context = {'items':items, 'order': order}
+    context = {'items':items, 'order': order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -30,12 +48,14 @@ def checkout(request):
         customer = request.user.customer
         order,create = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
         
     else:
         items = []
         order = {'get_cart_items': 0, 'get_cart_total': 0}
+        cartItems =order['get_cart_items']
         
-    context = {'items':items, 'order': order}
+    context = {'items':items, 'order': order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
 
 
