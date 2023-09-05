469905cbddfa369595100f3a30dedf03c5e07051
dawittamene <dawittamene559@gmail.com>
2023-09-05 23:35:11 +0300

    commit

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -69,13 +69,31 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
     if request.user.is_authenticated:
         customer = request.user.customer
         order,create = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        if total == float(order.get_cart_total):
+            order.complete = True
+            order.save()
+            
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                Phone=data['shipping']['Phone'],
+                home=data['shipping']['home'],
+            )    
     else:
         {
             print('user is not logged in....')
