4781c5196033765120405ddd1e4375047a070df3
dawittamene <dawittamene559@gmail.com>
2023-09-16 21:08:55 +0300

    commit

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -105,3 +105,33 @@ def processOrder(request):
 
 
 
+def signup(request):
+    if request.method == 'POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            messages.success(request, "account was created successfully!")
+            return redirect('loginpage')
+    return render(request, 'song/signup.html')
+def loginpage(request):
+    
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+           messages.info(request, "username or password is incorrect")
+    return render(request, 'song/login.html')
+def logoutPage(request):
+    logout(request)
+    return redirect('loginpage')
