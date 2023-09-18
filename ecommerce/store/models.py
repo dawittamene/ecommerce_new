c1880dba8fb28f1f9cee6d4338eadc087cd31379
dawittamene <dawittamene559@gmail.com>
2023-09-18 14:48:36 +0300

    commit

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Customer(models.Model):
-    user = models.ForeignKey(User, null=True , blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True , blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200)
     def __str__(self):
